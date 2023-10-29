64b7e74d72f955740736481825104202d3518421
QuentinDevPython <quentin.barthelemy.01@gmail.com>
2023-10-29 11:45:26 +0100

    feat: Implement control functions for the Task setters

--- src/task.py
+++ src/task.py
@@ -7,16 +7,86 @@ class Task:
     instances = []
 
     def __init__(
-        self, name: str, due_date: str, description: str = "",
-        completion: int = 0
+        self, name: str, due_date: str, description: str = "", completion: int = 0
     ):
         self.id = next(Task.id_task) + 1
-        self.name = name
-        self.due_date = due_date
-        self.description = description
-        self.completion = completion
+        self.name = ""
+        self.due_date = ""
+        self.description = ""
+        self.completion = 0
+
+        self.create_task(name, due_date, description, completion)
         Task.instances.append(self)
 
+    def create_task(self, name: str, due_date: str, description: str, completion: int):
+        self.set_name(name)
+        self.set_due_date(due_date)
+        self.set_description(description)
+        self.set_completion(completion)
+
+    def control_variable_is_string(func):
+        """
+        Decorator function for controlling if a variable is a string.
+
+        :param func: The function to be decorated.
+        :type func: callable
+        :return: The decorated function.
+        :rtype: callable
+        :raises ValueError: If the variable is not a string.
+        """
+
+        def is_string(self, variable):
+            if not isinstance(variable, str):
+                raise ValueError("Le paramètre doit être une chaîne de caractères.")
+            return func(self, variable)
+
+        return is_string
+
+    def control_variable_is_integer(func):
+        """
+        Decorator function for controlling if a variable is an integer.
+
+        :param func: The function to be decorated.
+        :type func: callable
+        :return: The decorated function.
+        :rtype: callable
+        :raises ValueError: If the variable is not an integer.
+        """
+
+        def is_integer(self, variable):
+            if not isinstance(variable, int):
+                raise ValueError("Le paramètre doit être un entier.")
+            return func(self, variable)
+
+        return is_integer
+
+    def control_name_validity(func):
+        """
+        Decorator function for controlling the validity of task names.
+
+        This decorator ensures that task names are not empty.
+
+        :param func: The function to be decorated.
+        :type func: callable
+        :return: The decorated function.
+        :rtype: callable
+        :raises ValueError: If the name is empty.
+        """
+
+        def set_name(self, name: str):
+            if not name:
+                raise ValueError("Le nom ne peut pas être vide.")
+            for task in Task.instances:
+                if task.name == name:
+                    raise ValueError(
+                        "Cette tâche existe déjà. Veuillez spécifier un autre nom."
+                    )
+            return func(self, name)
+
+        return set_name
+
+    @control_variable_is_string
+    @control_name_validity
     def set_name(self, name: str):
         """
         Sets the name of the task.
@@ -26,6 +96,47 @@ class Task:
         """
         self.name = name
 
+    def control_date_validity(func):
+        """
+        Decorator function for controlling the validity of due dates.
+
+        This decorator ensures that due dates are in the format 'DD/MM/YYYY', and they are set to today or a future date.
+
+        :param func: The function to be decorated.
+        :type func: callable
+        :return: The decorated function.
+        :rtype: callable
+        :raises ValueError: If the date format is incorrect or if the date is not today or a future date.
+        """
+
+        def set_due_date(self, due_date: str):
+            # Contrôler le format de la date
+            if not len(due_date.split("/")) == 3:
+                raise ValueError("La date doit être de la forme 'JJ/MM/YYYY'")
+            day, month, year = due_date.split("/")
+            if not len(day) == 2:
+                raise ValueError("Le jour doit être de la forme 'JJ' (deux chiffres)")
+            if not len(month) == 2:
+                raise ValueError("Le mois doit être de la forme 'MM' (deux chiffres)")
+            if not len(year) == 4:
+                raise ValueError(
+                    "L'année doit être de la forme 'YYYY' (quatres chiffres)"
+                )
+
+            # Contrôler que la date est définie sur le jour d'aujourd'hui ou un jour ultérieur
+            date = datetime.datetime(int(year), int(month), int(day)).date()
+            today_date = datetime.datetime.now().date()
+            if not date >= today_date:
+                raise ValueError(
+                    f"La date du jour doit être définie sur la date du jour ou une date future. Date : {due_date}"
+                )
+
+            return func(self, due_date)
+
+        return set_due_date
+
+    @control_variable_is_string
+    @control_date_validity
     def set_due_date(self, due_date: str):
         """
         Sets the due date of the task.
@@ -36,6 +147,30 @@ class Task:
         day, month, year = due_date.split("/")
         self.due_date = datetime.datetime(int(year), int(month), int(day))
 
+    def control_description_validity(func):
+        """
+        Decorator function for controlling the validity of task descriptions.
+
+        This decorator ensures that task descriptions do not exceed 100 characters in length.
+
+        :param func: The function to be decorated.
+        :type func: callable
+        :return: The decorated function.
+        :rtype: callable
+        :raises ValueError: If the description length exceeds 100 characters.
+        """
+
+        def set_description(self, description: str):
+            if not len(description) <= 100:
+                raise ValueError(
+                    "La description d'une tâche est limitée à 100 caractères"
+                )
+            return func(self, description)
+
+        return set_description
+
+    @control_variable_is_string
+    @control_description_validity
     def set_description(self, description: str):
         """
         Sets the description of the task.
@@ -45,6 +180,30 @@ class Task:
         """
         self.description = description
 
+    def control_completion_validity(func):
+        """
+        Decorator function for controlling the validity of completion percentage.
+
+        This decorator ensures that the completion percentage falls within the range of 0 to 100%.
+
+        :param func: The function to be decorated.
+        :type func: callable
+        :return: The decorated function.
+        :rtype: callable
+        :raises ValueError: If the completion percentage is not in the range [0, 100].
+        """
+
+        def set_completion(self, completion: int):
+            if (not completion >= 0) or (not completion <= 100):
+                raise ValueError(
+                    "Le taux de complétion doit être compris entre 0 et 100"
+                )
+            return func(self, completion)
+
+        return set_completion
+
+    @control_variable_is_integer
+    @control_completion_validity
     def set_completion(self, completion: int):
         """
         Sets the completion percentage of the task.
@@ -84,9 +243,7 @@ class Task:
         :return: A string representation of the task.
         :rtype: str
         """
-        return f"Tâche {self.id} - {self.name}\n=> A faire pour le \ 
-            {self.due_date.day}/{self.due_date.month}/{self.due_date.year}\n\
-            => Avancée : {self.completion}%"
+        return f"Tâche {self.id} - {self.name}\n=> A faire pour le {self.due_date.day}/{self.due_date.month}/{self.due_date.year}\n=> Avancée : {self.completion}%"
 
     def __str__(self):
         """
@@ -95,7 +252,4 @@ class Task:
         :return: A string representation of the task.
         :rtype: str
         """
-        return f"Tâche {self.id} - {self.name}\n=> A faire pour le \ 
-            {self.due_date.day}/{self.due_date.month}/{self.due_date.year}\n\
-            => Avancée : {self.completion}%"
-
+        return f"Tâche {self.id} - {self.name}\n=> A faire pour le {self.due_date.day}/{self.due_date.month}/{self.due_date.year}\n=> Avancée : {self.completion}%"
