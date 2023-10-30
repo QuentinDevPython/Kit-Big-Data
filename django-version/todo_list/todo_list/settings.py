0e3bf32288edfe014c7122c16ff4c1445780c592
vulca1n <r.chenna15@gmail.com>
2023-10-30 14:37:24 +0100

    pep8 modif5

--- django-version/todo_list/todo_list/settings.py
+++ django-version/todo_list/todo_list/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = {"django-insecure-ty%v-4x97$zbme__%l65t9tv&hn!mw&f_3i%wcqx69c$zl1o_z"}
+SECRET_KEY = {
+    "django-insecure-ty%v-4x97$zbme__%l65t9tv&hn!mw&f_3i%wcqx69c$zl1o_z"
+    }
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,13 +96,16 @@ AUTH_PASSWORD_VALIDATORS = [
         "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
     },
 ]
 
